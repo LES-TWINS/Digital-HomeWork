0c7b82631ffed46454e788bec6cbe93998dae109
LES-TWINS <temogagna@gmail.com>
2022-07-02 13:54:08 +0400

    homework 1 2 3

---
+++ lesson 1/app.js
@@ -0,0 +1,31 @@
+//            დავალება 1
+
+// let name = prompt("შეიყვანეთ სახელი");
+// let surName = prompt("შეიყვანეთ გვარი");
+// let age = prompt("შეიყვანეთ ასაკი");
+
+// console.log(`თქვენი სახელია ${name}, თქვენი გვარია ${surName}, და ასაკი ${age}`);
+
+
+
+
+//            დავალება 2
+
+//  let firstName = "Temo";
+//  let lastName = "Gagnidze";
+//  let mail = "temogagna@gmail.com";
+//  let age = 27;
+//  let gender = "male";   
+ 
+//  document.write(`სახელი - ${firstName}, გვარი - ${lastName}, მეილი - ${mail}, ასაკი - ${age}, სქესი - ${gender}`);
+
+
+
+
+
+//               დავალება 3
+
+
+// let fullName = prompt("შეიყვანეთ სრული სახელი");
+// let outPut = document.querySelector('h1');
+// outPut.innerHTML = fullName;
---
+++ lesson 2/app.js
@@ -0,0 +1,60 @@
+         
+//                     დავალება 1
+
+
+//  let userInput = Number(prompt("შეიყვანეთ ასაკი"));
+ 
+//  if(userInput >= 18){
+//    let name = prompt("შეიყვანეთ სახელი");
+//    let surName = prompt("შეიყვანეთ გვარი");
+//    console.log(`გამარჯობა ${name} ${surName}`);
+//  } else if(userInput < 18){
+//     console.log("თქვენ ხართ არასრულწლოვანი");
+//  }
+//   else{
+//     console.log("Wrong Command :(((");
+//  }
+
+
+         
+//              დავალება 2
+
+// let firstQuest = prompt("ვინ ჩაულაგა საჭმელი წითელქუდას ? ა) დედამ ბ) მგელმა ც) მამამ დ) დამ");
+// let secondQuest = prompt("რამდენია 11 მეტრი ? ა) 25 ბ) 10 ც) 15 დ) 11");
+// let thirdQuest = prompt("ჩვენ ვართ ? ა) საართველო ბ) საქართველო ც) სართველო დ) სათველო");
+// let result = 0;
+
+//    if(firstQuest == "ა"){
+//     result++;
+//    }
+
+//    if(secondQuest == "დ"){
+//     result++;
+//    }
+
+//    if(thirdQuest == "ბ"){
+//     result++;
+//    } 
+//  console.log(`თქვენ სწორად უპასუხეთ ${result} კითხვას`);
+
+
+
+
+
+//                 დავალება 3
+
+// let userInput = Number(prompt("შეიყვანეთ რიცხვი"));
+// let operator = prompt("აირჩიეთ არითმეტიკული ოპერატორი (+ - * /)");
+// let userInput2 = Number(prompt("შეიყვანეთ რიცხვი"));
+
+// if(operator == "+"){
+//     console.log(userInput + userInput2);
+// } else if(operator == "-"){
+//     console.log(userInput - userInput2);
+// } else if(operator == "*"){
+//     console.log(userInput * userInput2);
+// } else if(operator == "/"){
+//     console.log(userInput / userInput2);
+// } else{
+//     console.log("Wrong Command");
+// }
