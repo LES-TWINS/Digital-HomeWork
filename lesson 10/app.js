062804c916214f7d8572af7706b8a5d965c14448
LES-TWINS <temogagna@gmail.com>
2022-07-19 22:01:11 +0400

    homework 10

---
+++ lesson 10/app.js
@@ -0,0 +1,49 @@
+// დავალება 1 (0-დან 100-მდე ყველა 3ის ჯერადი რიცხვი)
+
+// let arr = [];
+// for(let i = 0; i < 100; i++){
+//  if(i % 3 == 0 && i != 0){
+//   arr.push(i)
+//  }
+// } console.log(arr);
+
+// დავალება 2 
+
+// let arr = ["ana","gega","tornike","Andro","giorgi"];
+// for(let i = 0; i < arr.length; i++){
+//   if(arr[i][0].toLowerCase() == "a"){
+//       console.log(arr[i])
+//   }
+// }
+
+
+
+
+// დავალება 3
+
+// let arr = [];
+// for(let i = 0; i < 50; i++){
+//   if(i % 2 == 0 || i % 5 == 0 ){
+//    if(i % 10 != 0 && i != 0){
+//    arr.push(i)
+//    }
+//  }
+
+// } console.log(arr);
+
+
+// დავალება 4 
+
+// let arr = [2, 121, 4341, 242, 562, 43, 182, 55, 32, 98, 326];
+// let count = 0;
+
+// for(let i = 0; i < arr.length; i++){
+//     for(let j = 1; j <= arr[i]; j++){
+//       if(arr[i] % j == 0){
+//           count++;
+//       }
+//     }
+//     console.log(`${arr[i]} -ის გამყოფების რაოდენობა არის ${count}`);
+//     count = 0;
+  
+// } 
