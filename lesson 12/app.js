626d14c61de52f7d6c7f10bcf280079b2f0669c0
LES-TWINS <temogagna@gmail.com>
2022-07-28 00:38:31 +0400

    homework 13

---
+++ lesson 12/app.js
@@ -0,0 +1,57 @@
+//   დავალება 1                  
+
+//  function makeRandNumArr(arr){
+//    while(arr.length < 20){
+//       arr.push(Math.round(Math.random() * 100))
+//    }
+//    console.log(arr)
+//  }
+//  let arr = [];
+// makeRandNumArr(arr);
+
+
+//  დავალება 2
+
+// function evenNum(arr){
+//     arr.forEach(element => {
+//       if(element % 2 == 0){
+//           console.log(element)
+//       }
+//     });
+// }
+
+// evenNum(arr);                                
+
+
+// დავალება 3
+
+// function getRandNumArr(count){
+//   let newArr = [];
+//   for(let i = 0; i < count; i++){
+//       newArr.push(Math.round(Math.random() * 100))
+//   }
+//   return newArr;
+// }
+
+// let returnedArr = getRandNumArr(15);
+// console.log(returnedArr);
+
+
+// დავალება 4
+
+// function getSomeNumsNewArr(arr){
+//     let newArr = arr.filter(element => element % 3 == 0);
+//     return newArr;
+// }
+
+// let returnedNewArr = getSomeNumsNewArr(arr);
+// console.log(returnedNewArr)
+
+
+// დავალება 5
+
+// function findNum(arr,num){
+//  return arr.includes(num)
+// }
+// let findedNum = findNum(arr,17);
+// console.log(findedNum);
