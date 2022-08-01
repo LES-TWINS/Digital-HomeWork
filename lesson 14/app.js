1f9cba257b8cd08436f84bdb12b3052fb9ca3653
LES-TWINS <temogagna@gmail.com>
2022-08-01 12:30:16 +0400

    homework 16

--- lesson 14/app.js
+++ lesson 14/app.js
@@ -117,17 +117,138 @@
 
 
 
+                       // საკლასო სამუშაო
+                  // object                      
+
+
+// let temo = {
+//     name:"temo",
+//     surname:"gagnidze",
+//     age:25,
+//     email:"temogagna@gmail.com"
+// }
+// console.log(temo.name);
+// temo.name = "temooo";
+// console.log(temo);
+// console.log(temo["name"]);
+
+
+// let human = {
+//     personalInfo : {
+//         name: "giorgi",
+//         lastName:"okruadze",
+//         age:25
+//     },
+//     job:{
+//         prof:"full stack developer",
+//         company:"space bank",
+//         programmingLang:["c#","angular","javascript"]
+//     }
+// }
+
+
+// console.log(human);
+// console.log(human.job);
+
+
+
+// let human = {
+//     name:"temo",
+//     surname:"gagnidze",
+//     age:25,
+//     getHumanInfo:function(){
+//         return `i am ${this.name} ${this.surname} old`
+//     },
+//     getInstance:function(){
+//         return this;
+//     }
+
+// }
 
 
 
 
 
+// console.log(human.getHumanInfo())
+// console.log(human.getInstance())
+
+
+
+// function getHuman(name,surname,age,email){
+//     return{
+//         name:name,
+//         surname:surname,
+//         age:age,
+//         email:email
+//     };
+// }
+
+// let temo = getHuman("temo","gagnidze",27,"temogagna@gmail.com");
+// let dato = getHuman("temo","gagnidze",12,"temogagna@gmail.com");
+// let gega = getHuman("temo","gagnidze",13,"temogagna@gmail.com");
+// let lewis = getHuman("temo","gagnidze",31,"temogagna@gmail.com");
+
+// let humans = [temo,dato,gega,lewis];
+
+// let filterByAge = humans.filter(item => item.age > 18);
+
+// console.log(filterByAge);
 
 
 
 
+ function human(name,lastName,age,email,gender,adress,country){
+    return{
+        firstname:name,
+        lastname:lastName,
+        age:age,
+        email:email,
+        gender:gender,
+        adress:adress,
+        country:country
+    };
+ }
+ let humans = [
+    human("temo","gagnidze",27,"temogagna@gmail.com","male","batumi","georgia"),
+    human("dato","gagnidze",27,"temogagna@gmail.com","male","tbilisi","georgia"),
+    human("zura","gagnidze",15,"temogagna@gmail.com","male","tbilisi","georgia"),
+    human("temo","gagnidze",13,"temogagna@gmail.com","male","gori","georgia")
+]
+console.log(humans)
 
 
+ function getFiltered(arr){
+    let filterByAge = arr.filter(item => item.age > 18);
+    return filterByAge;
+   
+ }
+
+ function getFilteredAdress(arr,adress){
+    return arr.filter(item => item.adress == adress)
+     
+   
+ }
+ let finishedAdd = getFilteredAdress(humans,"tbilisi");
+
+
+ function getName(arr,name){
+    return arr.filter(item => item.firstname == name)
+
+ }
+ let finishedName = getName(humans,"temo")
+
+ 
+ function getCollection (arr){
+  return arr.map(item =>{
+     return{
+        name:`${item.firstname} ${item.lastname}`,
+        age:item.age,
+        adress:`${item.adress}`,
+     }
+  })
+ }
+ let done = getCollection(humans)
+ console.log(done)
 
 
 
---
+++ lesson 16/app.js
@@ -0,0 +1,75 @@
+// დავალება 1
+
+// function getObj(name,lastName,age,meil){
+//  return{
+//     name:name,
+//     lastName:lastName,
+//     age:age,
+//     meil:meil
+//  }
+// }
+// let newObj = getObj("temo","gagnidze",27,"temogagna@gmail.com");
+// console.log(newObj);
+
+
+// დავალება 2
+
+
+// function getFullInfo(name,lastName,age,meil,school,university,workName,positionWork,workExperience,country,city,adress){
+// return{
+//     personalInfo:{
+//         name:name,
+//         lastName:lastName,
+//         age:age,
+//         meil:meil
+//     },
+//     education:{
+//         school:school,
+//         university:university
+//     },
+//     workExperience:{
+//         companyName:workName,
+//         currentPos:positionWork,
+//         workExp:workExperience
+//     },
+//     adress:{
+//         country:country,
+//         city:city,
+//         adress:adress
+//     }
+// }
+// }
+
+// let fullInfo = getFullInfo("temo","gagnidze",27,"temogagna@gmail.com","9-school","tsu","IT Step","lecturer","8 month","GEORGIA","tbilisi","wereteli");
+// console.log(fullInfo);
+
+
+// დავალება 3
+
+
+// function getAnimal(name,age,breed,gender){
+//     return{
+//         name:name,
+//         age:age,
+//         breed:breed,
+//         gender:gender
+//     }
+// }
+
+// let animals = [
+//   getAnimal("sem",5,"doberman","male"),
+//   getAnimal("Jon",8,"Shepherd","female"),
+//   getAnimal("steve",6,"bulldog","male")
+// ]
+
+// function getAnimalObj(obj,gender){
+//  return obj.filter(anim => anim.gender == gender);
+// }
+// let filteredAnimal = getAnimalObj(animals,"male");
+// console.log(filteredAnimal);
+
+
+
+// დავალება 4
+
+
