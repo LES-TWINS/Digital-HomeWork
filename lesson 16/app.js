bfbb1ff9d544c482d2bb9b9c739b9899812e1293
LES-TWINS <temogagna@gmail.com>
2022-08-02 16:43:12 +0400

    homework16

--- lesson 16/app.js
+++ lesson 16/app.js
@@ -70,6 +70,104 @@
 
 
 
+
+
 // დავალება 4
+//  A
+
+
+// function getHumanObj(name,lastName,age,gender,meil,phoneNum){
+// return{
+//     name:name,
+//     lastName:lastName,
+//     age:age,
+//     gender:gender,
+//     meil:meil,
+//     phoneNum:phoneNum
+// }
+// }
+
+// let humanArr = [
+//     getHumanObj("temo","gagnidze",27,"male",599858585)
+// ]
+
+
+// function getFullObj(collection){
+// return collection.map(item => {
+//     return {
+//         fullName: `${item.name} ${item.lastName}`,
+//         meil: `${item.meil}`
+//     }
+// })
+// }
+// let fullInfo = getFullObj(humanArr);
+// console.log(fullInfo);
+
+
+
+
+
+// B
+
+// function getContactObj(name,lastName,phoneNum,meil,img){
+//   return{
+//       name:name,
+//       lastName:lastName,
+//       phoneNum:phoneNum,
+//       meil:meil,
+//       img:img
+//   }
+// }
+// let list = [
+//     getContactObj("temo","gagnidze",574856212,"temogagna@gmail.com","https://media.istockphoto.com/photos/tbilisi-downtown-georgia-taken-in-april-2019-picture-id1160446488?k=20&m=1160446488&s=612x612&w=0&h=9C-ElsOf5azgVovdU3CarfiWKBlbUTDYKu1ysRAUH4w="),
+//     getContactObj("vako","gundishvili",574856312,"vakotest@gmail.com","https://media.istockphoto.com/photos/tbilisi-downtown-georgia-taken-in-april-2019-picture-id1160446488?k=20&m=1160446488&s=612x612&w=0&h=9C-ElsOf5azgVovdU3CarfiWKBlbUTDYKu1ysRAUH4w="),
+//     getContactObj("gega","rostomashvili",577856212,"gegarosto@gmail.com","https://media.istockphoto.com/photos/tbilisi-downtown-georgia-taken-in-april-2019-picture-id1160446488?k=20&m=1160446488&s=612x612&w=0&h=9C-ElsOf5azgVovdU3CarfiWKBlbUTDYKu1ysRAUH4w="),
+// ]
+// function getContactArr(collection,name,lastName){
+//  return collection.filter(item => item.name == name || item.lastName == lastName)
+// }
+// let filteredList = getContactArr(list,"vako","gagnidze");
+// console.log(filteredList);
+
+
+
+// // დავალება 5
+
+
+// function createUser(fullName,id,numsList){
+// return{
+//     fullName:fullName,
+//     id:id,
+//     numsList:numsList
+// }
+// }
+
+// let users = [
+//     createUser("temo gagnidze",25419854,[1,7,14,4,30,31,5]),
+//     createUser("vako gundishvili",25419854,[8,19,20,4,30,37,38]),
+//     createUser("gega rostomashvili",25419854,[2,7,12,4,11,37,9]),
+//     createUser("tornike rostomashvili",25419854,[3,7,13,45,29,31,5]),
+//     createUser("lewis hamilton",25419854,[3,40,6,4,30,31,8])
+// ]
+
+// let getLuckyNums = () => {
+//     let luckyNumsArr = [];
+//      while(luckyNumsArr.length < 7){
+//         luckyNumsArr.push(Math.floor(Math.random() * 49))
+//      }
+//     return luckyNumsArr;
+// }
+// let winnerNums = getLuckyNums();
+
+// let getWinner = (users,winnerNums) =>{
+//  for(let user of users){
+//    if(user.numsList.every(num => winnerNums.includes(num))){
+//       console.log(`თქვენ მოიგეთ !!! მოგებული ბილეთის სახელი და გვარია  ${user.fullName}`)
+// // გიო ვინაიდან რენდომულად ვყრი მომგებიან რიცხვებს მასივში ძაან ნაკლები შანსია რო დაემთხვეს უსერის ციფრებს
+// // მაგრამ ხელით რო ჩავყარო იგივე რიცხვები მუშაობს ყველანაირად გავტესტე :D
+//    }
+//  }
+// }
+// getWinner(users,winnerNums);
 
 
